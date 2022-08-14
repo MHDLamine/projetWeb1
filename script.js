a8bb91d90266c6fec21b5cbef974a7bc0d1250e8
Mouhamed Lamine NGOM <mouhamed.ngom50@gmail.com>
2022-08-14 12:10:47 +0000

    "getElementByClassName -> queryselectorAll"

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function play()
 {
     let couleur = [
-    "#7F00FF",      "#4D4DFF",	    "#871F78",
-    "#00009C",		"#6B238E",		"#23238E",
-    "#4F2F4F",		"#FF1CAE",      "#5959AB",
-    "#DB7093",		"#FF6EC7",		"#DB70DB",
-    "#EAADEA",		"#856363",		"#BC8F8F",
-    "#A68064",		"#97694F",		"#855E42",
-    "#A62A2A",		"#8E2323",		"#8C1717",
-    "#5C3317",		"#6B4226",		"#5C4033",
+    "#7F00FF", "#4D4DFF", "#871F78", "#00009C", "#6B238E", "#23238E", "#4F2F4F", "#FF1CAE", "#5959AB",
+    "#DB7093", "#FF6EC7", "#DB70DB", "#EAADEA", "#856363", "#BC8F8F","#A68064", "#97694F", "#855E42",
+    "#A62A2A","#8E2323",		"#8C1717", "#5C3317", "#6B4226", "#5C4033","#FFFF00", "#008000"
   ];
 
     let couleur1 = ( Math.floor(Math.random()  * couleur.length)) + 1;
@@ -16,9 +11,9 @@ function play()
  
 
     
-    var x = document.getElementsByClassName("bloc");
+    var x = document.querySelectorAll(".bloc");
     console.log(x);
-    var y = document.getElementsByClassName("para");
+    var y = document.querySelectorAll(".bloc p");
     for (var i=0; i < x.length; i++)
     {
         y[i].style.color = couleur [couleur1];
