fcd7bd072ca885ce838acf305dc837e277fcb5b0
Mouhamed Lamine NGOM <mouhamed.ngom50@gmail.com>
2022-08-14 18:12:08 +0000

    Multicolor

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function play()
 {
-    let couleur = [
-    "#7F00FF", "#4D4DFF", "#871F78", "#00009C", "#6B238E", "#23238E", "#4F2F4F", "#FF1CAE", "#5959AB",
-    "#DB7093", "#FF6EC7", "#DB70DB", "#EAADEA", "#856363", "#BC8F8F","#A68064", "#97694F", "#855E42",
-    "#A62A2A","#8E2323",		"#8C1717", "#5C3317", "#6B4226", "#5C4033","#FFFF00", "#008000"
-  ];
 
-    let couleur1 = ( Math.floor(Math.random()  * couleur.length)) + 1;
-    let couleur2 = ( Math.floor(Math.random()  * couleur.length)) + 1;
- 
+    /* On note 16 777 216 couleur possible donc je vai générer 
+    un nombre aléatoire entre 0 et ce nombre
+    */
+    let couleur1 = ( Math.floor(Math.random()  * 16777216) + 1);
+    let couleur2 = ( Math.floor(Math.random()  * 16777216) + 1);
 
+    // Je convertir la valeur en héxadécimal
+    let couleur_p = couleur1.toString(16);
+    let couleur_bloc = couleur2.toString(16);
     
-    var x = document.querySelectorAll(".bloc");
-    console.log(x);
-    var y = document.querySelectorAll(".bloc p");
+    // Je selectionne les élément à modifier
+    let x = document.querySelectorAll(".bloc");
+    let y = document.querySelectorAll(".bloc p");
     for (var i=0; i < x.length; i++)
     {
-        y[i].style.color = couleur [couleur1];
-        x[i].style.background = couleur [couleur2];
+        y[i].style.color = "#" + couleur_p;
+        x[i].style.background = "#" + couleur_bloc;
 
     }
-}
+  }
