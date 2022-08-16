f8de8750a7ab27103ffd7d70a5bf7fcb2a6e1c4c
Mouhamed Lamine NGOM <mouhamed.ngom50@gmail.com>
2022-08-16 14:09:35 +0000

    Modifier

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function play()
     /* On note 16 777 216 couleur possible donc je vai générer 
     un nombre aléatoire entre 0 et ce nombre
     */
-    let couleur1 = ( Math.floor(Math.random()  * 16777216) + 1);
-    let couleur2 = ( Math.floor(Math.random()  * 16777216) + 1);
+    let couleur1 = Math.floor(Math.random()  * 16777216) + 1;
+    let couleur2 = Math.floor(Math.random()  * 16777216) + 1;
 
     // Je convertir la valeur en héxadécimal
     let couleur_p = couleur1.toString(16);
